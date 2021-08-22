51944d49b6dd8748cf3928e6743dedf7c0a80297
Alexander <alexander.davidson87@gmail.com>
2021-08-22 16:51:38 -0500

    update

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -3,17 +3,26 @@ import PropTypes from "prop-types";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
+import axios from 'axios';
 
 export function LoginView(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password);
     /* Send a request to the server for authentication */
-    /* then call props.onLoggedIn(username) */
-    this.props.onLoggedIn(username);
+    axios.post('https://peaceful-forest-99574.herokuapp.com/https://myflix-cryptic-waters.herokuapp.com/login', {
+      Username: username,
+      Password: password
+    })
+      .then(response => {
+        const data = response.data;
+        props.onLoggedIn(authData);
+      })
+      .catch(e => {
+        console.log('no such user')
+      });
   };
 
   return (
@@ -21,12 +30,12 @@ export function LoginView(props) {
       <Form>
         <Form.Group controlId="formUsername">
           <Form.Label>Username:</Form.Label>
-          <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+          <Form.Control type="text" placeholder="Enter Username" value={username} onChange={e => setUsername(e.target.value)} />
         </Form.Group>
 
         <Form.Group controlId="formPassword">
           <Form.Label>Password:</Form.Label>
-          <Form.Control type="password" onChange={e => setPasswrod(e.target.value)} />
+          <Form.Control type="password" placeholder="Enter Password" value={password} onChange={e => setPassword(e.target.value)} />
         </Form.Group>
         <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
       </Form>
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -31,6 +31,20 @@ class MainView extends React.Component {
       });
   }
 
+  getMovies(token) {
+    axios.get('https://myflix-cryptic-waters.herokuapp.com/movies', {
+      headers: { Authorization: 'Bearer ${token}' }
+    })
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   /* When a movie is clicked, this funciton is invoked and updates the state of 'selectedMovie' *property to that movie */
   setSelectedMovie(movie) {
     this.setState({
@@ -39,20 +53,26 @@ class MainView extends React.Component {
   }
 
   /* When a user successfully logs in, this function updates the 'user' property in state to that *particular user*/
-  onLoggedIn(user) {
+  onLoggedIn(authData) {
+    console.log(authData);
     this.setState({
-      user
+      user: authData.user.Username
     });
+
+    localStorage.setItem('token', authData.token);
+    localStorage.setItem('user', authData.user.Username);
+    this.getMovies(authData.token);
   }
 
   render() {
     const { movies, selectedMovie, user, register } = this.state;
 
+    /* If there is no registration, the RegistrationView is rendered.  If there is a registered user, the useres details are *passed as a prop to the LoginView*/
+    // if (!register) return <RegistrationView onRegistration={(register) => this.onRegistration(register)} />;
+
     /* If there is no user, the LoginView is rendered.  If there is a user logged in, the user details are *passed as a prop to the LoginView*/
     if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
 
-    /* If there is no registration, the RegistrationView is rendered.  If there is a registered user, the useres details are *passed as a prop to the LoginView*/
-    if (!register) return <RegistrationView onRegistration={(register) => this.onRegistration(register)} />;
 
     // Before the movies have been loaded
     if (movies.length === 0) return <div className="main-view" />;
@@ -72,7 +92,8 @@ class MainView extends React.Component {
         }
       </Row >
     );
-  }
+  };
 }
 
 export default MainView
+
